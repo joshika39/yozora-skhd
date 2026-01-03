2e1a41b992ba6153b5cf85c89b2bbcb54c53de79
Joshua Hegedus <josh.hegedus@outlook.com>
2026-01-03 20:25:23 +0100

    fixed left right iterating scripts to work with wrapping

--- scripts/focus-display-left.sh
+++ scripts/focus-display-left.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env sh
-
 idx="$(yabai -m query --displays | jq -r '
   sort_by(.frame.x) as $d
-  | ($d | map(."has-focus") | index(true)) as $i
-  | ($d | length) as $n
-  | $d[((($i - 1) + $n) % $n)].index
+  | ($d | map(select(."has-focus")) | .[0]) as $cur
+  | ($cur.frame.x) as $cx
+  | ($d | map(select(.frame.x < $cx))) as $left
+  | if ($left|length) > 0 then $left[-1].index else $d[-1].index end
 ')"
 
 [ -n "$idx" ] && yabai -m display --focus "$idx"
